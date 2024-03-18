daf2400a24504416683647ef1c37b74d6b8ef897
Zhell <zhellqol@gmail.com>
2024-03-18 18:37:38 +0100

    Update code

--- module/setup.mjs
+++ module/setup.mjs
@@ -19,50 +19,26 @@ Hooks.once("init", function() {
     scope: "world",
     requiresReload: true
   });
-});
 
-function addExhaustion(actor, parts, setting) {
-  if (setting && !game.settings.get("alternative-exhaustion-5e", "manual")) return;
-  const exh = actor.system.attributes?.exhaustion ?? 0;
-  if (exh > 0) parts.push("-@attributes.exhaustion");
-}
-
-Hooks.on("dnd5e.preRollAttack", function(item, config) {
-  addExhaustion(item.actor, config.parts, true);
-});
-
-Hooks.on("dnd5e.preRollAbilityTest", function(actor, config) {
-  addExhaustion(actor, config.parts, true);
-});
-
-Hooks.on("dnd5e.preRollAbilitySave", function(actor, config) {
-  addExhaustion(actor, config.parts, true);
-});
-
-Hooks.on("dnd5e.preRollDeathSave", function(actor, config) {
-  addExhaustion(actor, config.parts, false);
-});
-
-Hooks.on("dnd5e.preRollSkill", function(actor, config) {
-  addExhaustion(actor, config.parts, true);
-});
-
-Hooks.on("dnd5e.preRollInitiative", function(actor, roll) {
-  // Non-functional.
-});
-
-Hooks.on("dnd5e.prepareLeveledSlots", function(spells, actor, progression) {
   if (game.settings.get("alternative-exhaustion-5e", "manual")) return;
-  const exh = actor.system.attributes?.exhaustion ?? 0;
-  if (!(exh > 0)) return;
-  for (const [k, v] of Object.entries(actor.system.skills ?? {})) {
-    v.total -= exh;
-    v.passive -= exh;
-  }
-  for (const [k, v] of Object.entries(actor.system.abilities ?? {})) {
-    v.mod -= exh;
-    v.save -= exh;
-    v.dc -= exh;
-  }
-  actor.system.attributes.spelldc -= exh;
+
+  Hooks.on("dnd5e.preRollDeathSave", function(actor, config) {
+    const exh = actor.system.attributes?.exhaustion ?? 0;
+    if (exh > 0) config.parts.push("-@attributes.exhaustion");
+  });
+
+  Hooks.on("dnd5e.prepareLeveledSlots", function(spells, actor, progression) {
+    const exh = actor.system.attributes?.exhaustion ?? 0;
+    if (!(exh > 0)) return;
+    for (const [k, v] of Object.entries(actor.system.skills ?? {})) {
+      v.total -= exh;
+      v.passive -= exh;
+    }
+    for (const [k, v] of Object.entries(actor.system.abilities ?? {})) {
+      v.mod -= exh;
+      v.save -= exh;
+      v.dc -= exh;
+    }
+    actor.system.attributes.spelldc -= exh;
+  });
 });
